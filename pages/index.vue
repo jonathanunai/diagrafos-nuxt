639cd7295dcede4297ceda7b283e244084612661
unai.me <me@unai.me>
2020-11-25 22:45:45 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <full-page ref="fullpage" :options="options" id="fullpage">
+      <div class="section">First section ...</div>
+      <div class="section">Second section ...</div>
+    </full-page>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        sectionsColor: ['#41b883', '#ff5f45', '#0798ec'],
+      },
+    }
+  },
+}
+</script>
